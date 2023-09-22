2982b45f23da01937a56f420ee89f29fca8500ee
prathamCVT <143216956+prathamCVT@users.noreply.github.com>
2023-09-22 15:33:48 -0400

    Add files via upload

---
+++ src/CommandProcessor.java
@@ -0,0 +1,18 @@
+
+public class CommandProcessor {
+    
+        private int id;
+        private String title;
+        private String date; // Seminar date
+        private int length; // Seminar length
+        private String[] keywords; // Seminar keywords
+        private short x; // Seminar x coord
+        private short y; // Seminar y coord
+        private String desc; // Seminar description
+        private int cost; // Seminar cost
+        private int hashSize;
+        private int memSize;
+        
+        public CommandProcessor
+
+}
