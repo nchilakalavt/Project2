16a4b0d2079c1e28393b25a0c87bc1afb5787fa8
nirishchilakala <nchilakala@vt.edu>
2023-09-23 14:41:56 -0400

    add

--- src/BST.java
+++ src/BST.java
@@ -3,45 +3,76 @@
 public class BST<K extends Comparable<K>, E> {
     private BSTNode<KVPair<K, E>> root;// Root of the BST
     private int nodecount; // Number of nodes in the BST
-    
-    
-    
+
     // constructor
     BST() {
         root = null;
         nodecount = 0;
     }
-    
-    
-    private Comparable findHelp(BSTNode<KVPair<K, E>> root, K e) {
+
+
+    private BSTNode<KVPair<K, E>> findHelp(
+        BSTNode<KVPair<K, E>> removeNode,
+        K e) {
         if (root == null) {
             return null;
         }
         if (e.compareTo(root.value().key()) == 0) {
-            return root.value();
+            return root;
         }
         else if (e.compareTo(root.value().key()) < 0) {
             findHelp(root.left(), e);
         }
-        
+
         else if (e.compareTo(root.value().key()) > 0) {
             findHelp(root.right(), e);
         }
         return null;
     }
-    
-    private BSTNode<KVPair<K, E>> removeHelp(
-        BSTNode<KVPair<K, E>> root,
-        K e) {
-        
-        return null;
+
+
+    private void removeHelp(BSTNode<KVPair<K, E>> root) {
+        if (root.isLeaf()) {
+            BSTNode<KVPair<K, E>> temp = root;
+            root = null;
+        }
+        else {
+            root.setValue(null);
+            reorder(root);
+        }
     }
-    
-    private BSTNode<KVPair<K, E>> insertHelp(
-        BSTNode<KVPair<K, E>> root,
-        K e) {
-        return null;
+
+
+    private void reorder(BSTNode<KVPair<K, E>> removeNode) {
+        if (removeNode.isLeaf()) {
+            removeNode = null;
+        }
+        else if (removeNode.left() == null) {
+            removeNode.setValue(removeNode.right().value());
+            removeNode.right().setValue(null);
+            reorder(removeNode.right());
+        }
+        else {
+            removeNode.setValue(removeNode.left().value());
+            removeNode.left().setValue(null);
+            reorder(removeNode.left());
+        }
     }
+
+
+    private void insertHelp(BSTNode<KVPair<K, E>> root, K e) {
+        if (root == null) {
+            root.setValue(e);
+        }
+        else if (e.compareTo(root.value().key()) == 0 || e.compareTo(root
+            .value().key()) < 0) {
+            insertHelp(root.left(), e);
+        }
+        else {
+            insertHelp(root.right(), e);
+        }
+    }
+
 
     // Reinitialize tree
     public void clear() {
@@ -54,32 +85,27 @@ public class BST<K extends Comparable<K>, E> {
     // Records can be anything, but they must be Comparable
     // e: The record to insert.
     public void insert(K e) {
-        root = insertHelp(root, e);
+        insertHelp(root, e);
         nodecount++;
     }
-
-
-
 
 
     // Remove a record from the tree
     // key: The key value of record to remove
     // Returns the record removed, null if there is none.
-    public Comparable remove(K key) {
-        Comparable temp = findHelp(root, key); // First find it
+    public BSTNode<KVPair<K, E>> remove(K key) {
+        BSTNode<KVPair<K, E>> temp = findHelp(root, key); // First find it
         if (temp != null) {
-            root = removeHelp(root, key); // Now remove it
+            removeHelp(temp); // Now remove it
             nodecount--;
         }
         return temp;
     }
 
 
-
-
     // Return the record with key value k, null if none exists
     // key: The key value to find
-    public Comparable find(K key) {
+    public BSTNode<KVPair<K, E>> find(K key) {
         return findHelp(root, key);
     }
 
---
+++ src/SemDatabase.java
@@ -0,0 +1,54 @@
+public class SemDatabase {
+
+    private int worldSize;
+
+    public SemDatabase(int w) {
+        worldSize = w;
+    }
+
+    public void searchID(int key) {
+
+    }
+    public void searchCost(int low, int high) {
+
+    }
+    public void searchKeyword(String keyWorder) {
+
+    }
+    public void searchLocation(int x, int y, double r) {
+
+    }
+    public void searchDate(int firstT, int secondT) {
+
+    }
+
+
+    public void delete(int key) {
+
+    }
+
+
+    public void printDate() {
+
+    }
+
+
+    public void printKeyword() {
+
+    }
+
+
+    public void printLocation() {
+
+    }
+
+
+    public void printCost() {
+
+    }
+
+
+    public void printID() {
+
+    }
+}
